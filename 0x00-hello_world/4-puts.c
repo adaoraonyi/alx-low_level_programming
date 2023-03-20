00323c01b1351a8a655c5dd82b39c1031198d9e1
adaoraonyi <adaedithugwu@gmail.com>
2023-03-20 07:11:35 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main - this is the main function
  *
- * Return:  is always(0)
+ * Return: is always(0)
  */
 
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle);
+	put("\"Programming is like building a multilingual puzzlei");
 	return (0);
 }
